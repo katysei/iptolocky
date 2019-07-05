45e04de697c4209c7933cfa82978a832b3406d20
jossiekat <jossiekat@icloud.com>
2019-07-05 23:30:48 +0300

    creating ip loc string- now just return it

--- ip_to_loc_server.rb
+++ ip_to_loc_server.rb
@@ -1,3 +1,4 @@
+require 'sinatra'
 require_relative 'ip_to_loc'
 
 ip_to_loc = IpToLoc.new 'IP2LOCATION-LITE-DB1.BIN'
@@ -6,3 +7,15 @@ loc = ip_to_loc.get_loc '37.142.3.28'
 
 puts  'Country Code: ' + loc.country_short + "\n"
 print 'Country Name: ' + loc.country_long + "\n"
+
+
+get '/' do
+  ip = params[:ip]
+  if ip.nil?
+  	"Nil"
+  else
+  	loc = ip_to_loc.get_loc ip
+  	"IP: #{ip} Country Name: #{loc.country_long}" 
+
+  end
+end
