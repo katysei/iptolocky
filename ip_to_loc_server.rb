8bf7bdef324c8d7ca7c2b5e71a66c470a3c357c3
katysei <katysei@gmail.com>
2019-07-05 16:02:39 +0300

    Update ip_to_loc_server.rb

--- ip_to_loc_server.rb
+++ ip_to_loc_server.rb
@@ -4,5 +4,5 @@ ip_to_loc = IpToLoc.new 'IP2LOCATION-LITE-DB1.BIN'
 
 loc = ip_to_loc.get_loc '37.142.3.28'
 
-puts  'Country Code: ' + loc.country_short + "\n"
-print 'Country Name: ' + loc.country_long + "\n"
+puts 'Country Code: ' + loc.country_short
+puts 'Country Name: ' + loc.country_long
